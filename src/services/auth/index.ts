263d70381b88e407122662194707c6e88f4b0cca
Raí Joia <megac154@gmail.com>
2024-03-08 00:32:48 -0300

    feat: next-auth ok com toaster de notification

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,9 +1,24 @@
-import NextAuth from 'next-auth'
-import Github from 'next-auth/providers/github'
+import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '../database';
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
-  providers: [Github],
-})
+  pages: {
+    signIn: '/auth',
+    signOut: '/auth',
+    error: '/auth',
+    verifyRequest: '/auth',
+    newUser: '/app',
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+});
